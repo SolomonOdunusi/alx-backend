4fb3e43d86635a7a763cfbea399867f239703c19
SolomonOdunusi <solomonodunusi@gmail.com>
2024-03-31 18:19:11 +0000

    Debugged hypermedia pagination script

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-"""get_hyper_index module"""
+"""
+get_hyper_index module
+"""
 import csv
+import math
 from typing import List, Dict, Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple:
+def index_range(page: int, page_size: int) -> tuple:
     """Calculate start and end indices for pagination."""
 
     start_index = (page - 1) * page_size
@@ -13,14 +16,16 @@ def index_range(page: int, page_size: int) -> Tuple:
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset."""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -30,41 +35,38 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return the appropriate page of the dataset."""
+        """Return the appropriate page of the dataset
+        """
         if (
             not isinstance(page, int) or
             not isinstance(page_size, int) or
             page <= 0 or page_size <= 0
         ):
             raise AssertionError
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
 
-        start_index, end_index = index_range(page, page_size)
-        dataset = self.dataset()
-        return dataset[start_index:end_index]
-
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+    def get_hyper(
+        self,
+        page: int = 1,
+        page_size: int = 10
+    ) -> Dict:
         """
         Implement a get_hyper method
         Return:
-
         page: the current page number
         data: the dataset page (equivalent to return from previous task)
         next_page: number of the next page, None if no next page
         prev_page: number of the previous page, None if no previous page
         total_pages: the total number of pages in the dataset as an integer
         """
-        dataset = self.dataset()
-        total_pages = len(dataset) // page_size +
-        (len(dataset) % page_size > 0)
-        start_index, end_index = index_range(page, page_size)
-        data = dataset[start_index:end_index]
-        next_page = page + 1 if end_index < len(dataset) else None
-        prev_page = page - 1 if start_index > 0 else None
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+
         return {
-            'page_size': len(data),
+            'page_size': len(self.get_page(page, page_size)),
             'page': page,
-            'data': data,
-            'next_page': next_page,
-            'prev_page': prev_page,
-            'total_pages': total_pages
+            'data': self.get_page(page, page_size),
+            'next_page': page + 1 if page < total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages,
         }
