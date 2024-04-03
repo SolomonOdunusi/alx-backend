0f19416141cbdb6347ee92c8cc4b0393c33c8b32
SolomonOdunusi <solomonodunusi@gmail.com>
2024-04-03 19:17:34 +0000

    Fixed fifo caching script

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -5,22 +5,15 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class FIFOCache(BaseCaching):
     """FIFO caching method"""
-    def __init__(self):
-        super().__init__()
-
     def put(self, key, item):
         """Input the data into the cache"""
-        if key is None or item is None:
-            return
-        if len(self.cache_data) > self.MAX_ITEMS:
-            first_item = next(iter(self.cache_data))
-            print("DISCARD: ", first_item)
-            del self.cache_data[first_item]
-
-        self.cache_data[key] = item
+        if key and item:
+            self.cache_data[key] = item
+            if len(self.cache_data) > self.MAX_ITEMS:
+                discard = list(self.cache_data)[0]
+                self.cache_data.pop(discard)
+                print("DISCARD: {}".format(discard))
 
     def get(self, key):
         """Retrieve the data from the cache"""
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key)
