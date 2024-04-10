aa11da47705c7d3ba9b3e4800fdb8d4464b9fbf3
SolomonOdunusi <solomonodunusi@gmail.com>
2024-04-10 01:45:52 +0000

    Added the jinja and python scripts

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Create a single / route"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__, template_folder="templates")
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def route_index():
+    """Renders a Basic Template"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Create a single / route"""
+from flask import Flask, render_template
+from flask_babel import Babel
+from flask import request
+
+
+app = Flask(__name__, template_folder="templates")
+babel = Babel(app)
+
+
+class Config(object):
+    """Config the app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def route_index():
+    """Render a Basic Template"""
+    return render_template('4-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Locale Selector func"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
